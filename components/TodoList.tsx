bfe209e7f57d97cc2a2edd121e3c9d2a651df062
Gabriel Chartier <gabriel@chartier.link>
2022-10-17 08:34:21 -0400

    Add basic components for todos

---
+++ components/TodoList.tsx
@@ -0,0 +1,38 @@
+import { StateObject } from "@hyper-hyper-space/react";
+import { HHSTodoList } from "../types/HHSTodo";
+import { TodoItem } from "../types/Todo";
+import { TodoListItem } from "./TodoListItem";
+
+interface Props {
+    listHeading: string;
+    emptyListMessage?: string;
+    todoList: StateObject<HHSTodoList>;
+    filter?: (todo: TodoItem) => boolean;
+}
+
+export function TodoList({
+    listHeading,
+    emptyListMessage = "No todos...",
+    todoList,
+    filter,
+}: Props) {
+    const todos = todoList?.getValue()?.items.getValue() ?? [];
+
+    return (
+        <fieldset>
+            <legend className="text-lg font-medium text-gray-900">{listHeading}</legend>
+
+            <ol
+                className={`mt-4 divide-y border-t divide-gray-200 border-gray-200 ${
+                    todos.length > 0 ? "border-b" : ""
+                }`}
+            >
+                {todos.length > 0 ? (
+                    todos.map((todo) => <TodoListItem todo={todo} todoList={todoList} />)
+                ) : (
+                    <p className="text-gray-400 pt-2">{emptyListMessage}</p>
+                )}
+            </ol>
+        </fieldset>
+    );
+}
---
+++ components/TodoListItem.tsx
@@ -0,0 +1,44 @@
+import { StateObject } from "@hyper-hyper-space/react";
+import { Dispatch, SetStateAction } from "react";
+import { HHSTodoList } from "../types/HHSTodo";
+import { TodoItem } from "../types/Todo";
+
+interface Props {
+    todo: TodoItem;
+    todoList: StateObject<HHSTodoList>;
+}
+
+export function TodoListItem({ todo, todoList }: Props) {
+    const { id, title } = todo;
+    const todos = todoList?.getValue()?.items.getValue() ?? [];
+
+    return (
+        <div key={id} className="relative flex items-start py-4">
+            <div className="min-w-0 flex-1 text-sm">
+                <label htmlFor={`todo-${id}`} className="select-none font-medium text-gray-700">
+                    {title}
+                </label>
+            </div>
+            <div className="ml-3 flex h-5 items-center">
+                <input
+                    id={`todo-${id}`}
+                    name={`todo-${id}`}
+                    type="checkbox"
+                    checked={todo.isCompleted}
+                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                    onClick={() => {
+                        todoList.getValue()?.items.setValue(
+                            todos.map((todo) => {
+                                if (todo.id === id) {
+                                    return { ...todo, isCompleted: !todo.isCompleted };
+                                }
+
+                                return todo;
+                            })
+                        );
+                    }}
+                />
+            </div>
+        </div>
+    );
+}
