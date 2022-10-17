ba6aa778fb90a84f1f7b9baafd537aa1662ca99c
Gabriel Chartier <gabriel@chartier.link>
2022-10-17 08:35:27 -0400

    Update index with todo list ui and functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,64 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { useObjectState } from "@hyper-hyper-space/react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useTodoListResources } from "../hooks/useTodoListResources";
+import { HHSTodoList } from "../types/HHSTodo";
+import { TodoItem } from "../types/Todo";
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const router = useRouter();
+    const todoList = useObjectState<HHSTodoList>();
+    const { data: todoListResources, isError, error, isLoading } = useTodoListResources();
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+    if (!todoList || isLoading) return <div>Loading.</div>;
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+    if (isError) return <div>ERROR</div>;
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+    if (router.query?.hash) {
+        router.push(`/todo-list/${router.query.hash}`);
+    }
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+            <Head>
+                <title>HHS Todo List App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+            <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+                <h1 className="text-3xl font-bold pt-8">🚀 HyperHyperSpace Todo List ✅</h1>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                <div className="my-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+                    <button
+                        onClick={async () => {
+                            const todoList = new HHSTodoList();
+                            const hash = todoList.hash();
+                            const hashQueryParam = encodeURIComponent(hash);
+                            alert(`Your todo list hash is: ${hash}`);
+                            await todoListResources.store.save(todoList);
+                            router.push(`/todo-list/${hashQueryParam}`);
+                        }}
+                        className="text-center mt-6 w-96 rounded-xl border p-6 hover:text-blue-600 focus:text-blue-600"
+                    >
+                        Add a Todo List!
+                    </button>
+                </div>
+            </main>
+
+            <footer className="flex h-24 w-full items-center justify-center border-t">
+                <a
+                    className="flex items-center justify-center gap-2"
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                </a>
+            </footer>
         </div>
-      </main>
+    );
+};
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
-}
-
-export default Home
+export default Home;
