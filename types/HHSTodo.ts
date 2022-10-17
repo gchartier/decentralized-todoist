d04a86cf3efb39a98d1e967e701013ed8e5db6b6
Gabriel Chartier <gabriel@chartier.link>
2022-10-17 08:33:56 -0400

    Add Todo and HHSTodo types

---
+++ types/HHSTodo.ts
@@ -0,0 +1,56 @@
+import {
+    ClassRegistry,
+    HashedObject,
+    MeshNode,
+    MutableReference,
+    Resources,
+    SpaceEntryPoint,
+} from "@hyper-hyper-space/core";
+import { TodoItem } from "./Todo";
+
+export class HHSTodoList extends HashedObject implements SpaceEntryPoint {
+    static className = "HHSTodo";
+    items: MutableReference<Array<TodoItem>>;
+
+    _node?: MeshNode;
+
+    constructor() {
+        super();
+
+        this.items = new MutableReference();
+    }
+
+    init(): void {
+        return;
+    }
+
+    getClassName(): string {
+        return HHSTodoList.className;
+    }
+
+    validate(): Promise<boolean> {
+        return Promise.resolve(true);
+    }
+
+    startSync(): Promise<void> {
+        if (this._node === undefined) {
+            this._node = new MeshNode(this.getResources() as Resources);
+            this._node.sync(this);
+            this._node.broadcast(this);
+        }
+
+        return Promise.resolve();
+    }
+
+    stopSync(): Promise<void> {
+        if (this._node !== undefined) {
+            this._node.stopSync(this);
+            this._node.stopBroadcast(this);
+            this._node = undefined;
+        }
+
+        return Promise.resolve();
+    }
+}
+
+ClassRegistry.register(HHSTodoList.className, HHSTodoList);
---
+++ types/Todo.ts
@@ -0,0 +1,6 @@
+export type TodoItem = {
+    id: number;
+    title: string;
+    description?: string;
+    isCompleted: boolean;
+};
